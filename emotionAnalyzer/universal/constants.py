4b6e8d71739351c8acf6e9a0dc146a5db42b6ae6
RichardHemphill <richardlhemphill@gmail.com>
2021-05-08 10:28:44 -0400

    file access

--- emotionAnalyzer/universal/constants.py
+++ emotionAnalyzer/universal/constants.py
@@ -4,7 +4,7 @@ FRAME_PERIOD = 100                          # time (ms) in analysis window
 HOP_PERIOD = 20                             # time between successive frames
 NUM_CHANNELS = 1                            # 1=monoaural, 2=stereophonic
 DATA_TYPE = 'int16'                         # sample type (16-bit integer)
-PROCESS_FILE = False                        # allow user to process files
+PROCESS_FILE = True                         # allow user to process files
 
 # calculated values
 FRAME_DURATION = FRAME_PERIOD / 1000        # fraction of second for frame
