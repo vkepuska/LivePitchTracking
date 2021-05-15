65fe524e8f522ac735f51b961521e6299ab8d6d6
RichardHemphill <richardlhemphill@gmail.com>
2021-05-15 19:06:30 -0400

    try calling predict_emotion

--- emotionAnalyzer/processing/emotionPredictor.py
+++ emotionAnalyzer/processing/emotionPredictor.py
@@ -2,17 +2,23 @@ from patterns.singleton import Singleton            # design pattern (one instan
 from enumerations.emotions import Emotion           # available emotions
 from enumerations.emotions import Intensity         # available intensity
 from gui.emotionForm import EmotionForm             # display emotion intensity
+from predictions.predictorv0 import predict_emotion
 
 class EmotionPredictor(object, metaclass=Singleton):
     def __init__(self):
         self.__emotionForm = None
         self.__getEmotionForm()
 
+    def predict(self, filename):
+        results = predict_emotion(filename)
+        print(results)
+        self.__updateEmotion()
+
     def __getEmotionForm(self):
         if self.__emotionForm is None:
             self.__emotionForm = EmotionForm.instance
 
-    def predict(self, file):
+    def __updateEmotion(self):
         self.__getEmotionForm()
         self.__emotionForm.emotion(Emotion.ANGRY)
         self.__emotionForm.intensity(Intensity.STRONG)
