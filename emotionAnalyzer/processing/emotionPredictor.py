97756f88c767efdf6e255c88dfa2df39526da576
RichardHemphill <richardlhemphill@gmail.com>
2021-05-16 08:26:39 -0400

    document code

--- emotionAnalyzer/processing/emotionPredictor.py
+++ emotionAnalyzer/processing/emotionPredictor.py
@@ -2,23 +2,29 @@ from patterns.singleton import Singleton            # design pattern (one instan
 from enumerations.emotions import Emotion           # available emotions
 from enumerations.emotions import Intensity         # available intensity
 from gui.emotionForm import EmotionForm             # display emotion intensity
-#from predictions.predictorv0 import predict_emotion
+from predictions.predictorv0 import predict_emotion # predict emotion via sound file
 
 class EmotionPredictor(object, metaclass=Singleton):
     def __init__(self):
+        """Construct object."""
         self.__emotionForm = None
         self.__getEmotionForm()
 
     def predict(self, filename):
-        #results = predict_emotion(filename)
-        #print(results)
+        """Predict emotion/intensity via convolutional neutal network."""
+        # filename    sound file to be analyzed for emotion
+
+        results = predict_emotion(filename)
+        print(results)
         self.__updateEmotion()
 
     def __getEmotionForm(self):
+        """Get the widget instance for diplaying emotion."""
         if self.__emotionForm is None:
             self.__emotionForm = EmotionForm.instance
 
     def __updateEmotion(self):
+        """Set the emotion/intensity displayed."""
         self.__getEmotionForm()
         self.__emotionForm.emotion(Emotion.ANGRY)
         self.__emotionForm.intensity(Intensity.STRONG)
