843281d41fc150b87fd02915cb7ca2c686c520a5
RichardHemphill <richardlhemphill@gmail.com>
2021-05-24 21:47:58 -0400

    higher fidelity

--- emotionAnalyzer/universal/constants.py
+++ emotionAnalyzer/universal/constants.py
@@ -2,14 +2,14 @@ from enumerations.emotions import Emotion
 from enumerations.emotions import Intensity
 
 # constant values
-FS = 16000                                  # sampling frequency (Hz)
+FS = 44100                                  # sampling frequency (Hz)
 FRAME_PERIOD = 100                          # time (ms) in analysis window
 NUM_CHANNELS = 1                            # 1=monoaural, 2=stereophonic
 DATA_TYPE = 'int16'                         # sample type (16-bit integer)
 RECORDING = 'recording'                     # name of folder to hold recording
 FILE_NAME_PREFIX = 'SAE'                    # designater for recording from app
 RECORDING_LENGTH = 5                        # seconds to record using mic
-FRAMES_PER_BUFFER = 1024                     # number of frames per buffer
+FRAMES_PER_BUFFER = 1024                    # number of frames per buffer
 
 # defaults
 DEFAULT_EMOTION = Emotion.NEUTRAL           # emotion to load at start
