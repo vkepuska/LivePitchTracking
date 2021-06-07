e060a6ef144a79c0a4546a682364bb1c3156f691
Ubuntu <wmckinnon2012@CTF-DEV.s5s4lvgnin2u5aqrkryo3zqvre.bx.internal.cloudapp.net>
2021-06-07 18:26:48 +0000

    We need to add a git ignore to ignore build files

---
+++ AndroidWork/KIVY/ANDRIOD/installRequirments.bash
@@ -0,0 +1,20 @@
+sudo apt-get -y udpdate
+sudo apt-get -y upgrade
+
+sudo apt-get -y install libffi-dev
+sudo apt-get -y install zlib1g-dev
+sudo apt-get -y install git
+#sudo apt-get -y install cython
+sudo apt-get -y install cython3
+sudo apt-get -y install openjdk-8-jdk
+sudo apt-get -y install make
+sudo apt-get -y install cmake
+sudo apt-get install -y dh-autoreconf
+sudo apt-get -y install build-essential python-dev python-setuptools -pyhon-pip python-smbus
+sudo apt-get -y install libncursesw5-dev libgdbm-dev libc6-dev
+sudo apt-get -y install libsqlite3-dev tk-dev
+sudo apt-get -y install libssl-dev openssl
+sudo apt-get -y install zip
+
+pip install Cython
+pip install certifi
